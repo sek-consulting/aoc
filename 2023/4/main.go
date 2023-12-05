cb1f6850a7a78b868a9837882a53f8ae6fda4c4c
Stefan E-K <stefan.eideloth@gmail.com>
2023-12-05 15:56:04 +0100

    day 4 part 2

--- 2023/4/main.go
+++ 2023/4/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"slices"
 	"strconv"
 	"strings"
@@ -14,7 +15,10 @@ func main() {
 	// result1 := part1("input.txt")
 	// fmt.Println(result1)
 
-	lib.Expect(part2("example2.txt"), "30")
+	// lib.Expect(part2("example1.txt"), "30")
+
+	result2 := part2("input.txt")
+	fmt.Println(result2)
 }
 
 func part1(filename string) string {
@@ -47,5 +51,35 @@ func countMatches(numbers, winners []string) int {
 }
 
 func part2(filename string) string {
-	return ""
+
+	var wins []int
+	for _, line := range lib.ReadLines(filename) {
+		split := strings.Split(line, ": ")
+		split = strings.Split(split[1], " | ")
+
+		winners := strings.Split(split[0], " ")
+		numbers := strings.Split(split[1], " ")
+		wins = append(wins, countMatches(numbers, winners))
+	}
+	// fmt.Println(wins)
+
+	tickets := make([]int, len(wins))
+	for i := range tickets {
+		tickets[i] += 1
+
+		// fmt.Println(i, "factor", tickets[i], "wins", wins[i])
+
+		for j := 1; j <= wins[i]; j++ {
+			if i+j < len(tickets) {
+				// fmt.Println("set", i+j, tickets[i+j], "+", tickets[i])
+				tickets[i+j] += tickets[i]
+			}
+		}
+
+		// fmt.Println(tickets)
+	}
+
+	sum := lib.SumSlice(tickets)
+
+	return strconv.Itoa(sum)
 }
